e8f9124ec2a791cd93debb0625587e9bb1d76487
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-06-26 08:04:31 +0530

    Update misc.py

--- ShasaBot/modules/helper_funcs/misc.py
+++ ShasaBot/modules/helper_funcs/misc.py
@@ -66,10 +66,10 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > 7:
-        pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
+    if len(pairs) > 6:
+        pairs = pairs[modulo_page * 6:6 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("☜", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("♔", callback_data="shasa_back"),
+                EqInlineKeyboardButton("༒𝐁𝐀𝐂𝐊༒", callback_data="shasa_back"),
              EqInlineKeyboardButton("☞", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
