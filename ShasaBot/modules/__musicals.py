15295fdfdcd904bc157b7bce843b49d1c569100a
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-06-26 07:55:31 +0530

    Update __musicals.py

--- ShasaBot/modules/__musicals.py
+++ ShasaBot/modules/__musicals.py
@@ -6,4 +6,5 @@ __help__ = """
 /music <i>query</i>: download song from yt servers. (API BASED)
 /lyrics <i>song name</i> : This plugin searches for song lyrics with song name.
 /glyrics <i> song name </i> : This plugin searches for song lyrics with song name and artist.
+/VCPLAYER : this command will show you help menu of vc player
 """
