3c0159f744deb1fc94f435ebd6d3ddc6fae09431
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-06-26 07:38:16 +0530

    Create Vcmusic.py

---
+++ ShasaBot/modules/Vcmusic.py
@@ -0,0 +1,38 @@
+__mod_name__ = "VC player"
+__help__ = """
+          》** VCPLAYER ** 《
+            
+ PATRICIA_VCPLAYER plays music in your group's voice chat
+ 
+ Assistant name >> ꧁ @PATRICIAXMUSIC ꧂
+ 
+⚙️Setting up 
+1) Make bot admin
+2) Start a voice chat
+3) Try `/play [song name]` for the first time by an admin
+*) If userbot joined enjoy music, If not add @PATRICIAXMUSIC to your group and retry
+  Commands
+
+=>> Song Playing 🎶
+  /play: Play song using youtube music
+  /play [yt url] : Play the given yt url
+  /dplay: Play song via deezer
+  /splay: Play song via jio saavn
+  
+=>> Playback ⏯
+  /player: Open Settings menu of player
+  /skip: Skips the current track
+  /pause: Pause track
+  /resume: Resumes the paused track
+  /end: Stops media playback
+  /current: Shows the current Playing track
+  /playlist: Shows playlist
+
+=>> More tools 📲
+  /admincache: Updates admin info of your group. Try if bot isn't recognize admin
+  /userbotjoin: Invite @PATRICIAXMUSIC Userbot to your chat
+
+*Player cmd and all other cmds except /play, /current  and /playlist are only for admins with manage group
+
+PLEASE NOTE THIS SERVICE IS UNSTABLE AND CAN BE STOPPED ANYTIME."""
+
