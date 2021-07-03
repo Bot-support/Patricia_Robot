5867487c878a43c38810c46334e97ddf60ccb8b1
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-07-03 17:43:14 +0530

    Update misc.py

--- ShasaBot/modules/helper_funcs/misc.py
+++ ShasaBot/modules/helper_funcs/misc.py
@@ -70,7 +70,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
         pairs = pairs[modulo_page * 8:6 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("☜", callback_data="{}_prev({})".format(prefix, modulo_page)),
              EqInlineKeyboardButton("༒𝐁𝐀𝐂𝐊༒", callback_data="shasa_back"),
-             EqInlineKeyboardButton("☞", callback_data="{}_next({})".format(prefix, modulo_page),)))]
+             EqInlineKeyboardButton("☞", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
         pairs += [[EqInlineKeyboardButton("Home", callback_data="bot_start")]]
